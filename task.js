db5b228ccf7cc9f01fe8a12a0569b5e741f0c814
SlavkaSlavka <56847146+SlavkaSlavka@users.noreply.github.com>
2021-08-12 15:19:21 +0300

    all, but not slider

--- task.js
+++ task.js
@@ -2,8 +2,13 @@ import { default as galleryItems } from './app.js';
 
 const galleryListEl = document.querySelector('ul.gallery');
 const galleryMarkup = createGalleryItemsMarkup(galleryItems);
-console.log(galleryMarkup);
+const lightBoxEl = document.querySelector('.lightbox');
+const lightboxImageEl = document.querySelector('.lightbox__image');
+const buttonCloseEl = document.querySelector('button[data-action="close-lightbox"]');
+const lightBoxOverlayEl = document.querySelector('div.lightbox__overlay');
+console.log(lightBoxOverlayEl);
 galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
 
 function createGalleryItemsMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
@@ -23,5 +28,36 @@ function createGalleryItemsMarkup(galleryItems) {
 </li>
     `
     }).join('');
+
 };
 
+
+galleryListEl.addEventListener('click', onGallertyListClick);
+
+function onGallertyListClick(evt) {
+  evt.preventDefault();
+  const isGalleryImageEl = evt.target.classList.contains('gallery__image');
+  if (!isGalleryImageEl) {
+    return
+  }
+  lightBoxEl.classList.add('is-open');
+  lightboxImageEl.src = evt.target.dataset.source;
+  lightboxImageEl.alt = evt.target.alt;
+
+}
+
+buttonCloseEl.addEventListener('click', onModalClose);
+function onModalClose() {
+  lightBoxEl.classList.remove('is-open');
+  lightboxImageEl.src = '';
+  lightboxImageEl.alt = '';
+}
+
+window.addEventListener(`keydown`, onModalClosebyEsc);
+function onModalClosebyEsc(e) {
+   if(e.code === 'Escape'){
+     onModalClose();
+  }
+}
+
+lightBoxOverlayEl.addEventListener('click', onModalClose);
